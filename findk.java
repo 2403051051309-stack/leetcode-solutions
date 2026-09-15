cb2759b42b039a6386eb59621922ea69784aedf2
priyam dwivedi <priyamdwivedi@priyams-MacBook-Air.local>
2026-09-16 03:06:49 +0530

    Add Find kth largest number

---
+++ findk.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+
+        // kth largest = (n-k)th smallest
+        int targetIndex = nums.length - k;
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int pivotIndex = partition(nums, left, right);
+
+            if (pivotIndex == targetIndex) {
+                return nums[pivotIndex];
+            }
+            else if (pivotIndex < targetIndex) {
+                left = pivotIndex + 1;
+            }
+            else {
+                right = pivotIndex - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private int partition(int[] nums, int left, int right) {
+
+        int pivot = nums[right];
+
+        int i = left;
+
+        for (int j = left; j < right; j++) {
+
+            if (nums[j] < pivot) {
+
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                i++;
+            }
+        }
+
+        int temp = nums[i];
+        nums[i] = nums[right];
+        nums[right] = temp;
+
+        return i;
+    }
+}
